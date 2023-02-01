a4fcf30cefd4560a03cbafe7729e6c3e1651d748
David Chi <davidchi31415@gmail.com>
2023-01-31 19:43:33 -0600

    Reformatted directory

--- toggle_style.js
+++ toggle_style.js
@@ -8,8 +8,8 @@ function toggleStyleSheet(){
     let currentStyle = styleElement.getAttribute("href");
 
     // 1 (c) Determine new stylesheet file name
-    let newStyle = "../styles.css";
-    if (currentStyle === "../styles.css") newStyle = "../other_styles.css";
+    let newStyle = "styles.css";
+    if (currentStyle === "styles.css") newStyle = "other_styles.css";
 
     // 1 (d) replace stylesheet with new stylesheet (hint: element.setAttribute)
     styleElement.setAttribute("href", newStyle);
@@ -29,8 +29,8 @@ window.onload = function(){
     // 2 (a) get stylesheet name from local storage hint: localStorage.getItem(name)
     let preferredStyle = localStorage.getItem("preferredStyle");
     
-    if (preferredStyle !== "../styles.css" && preferredStyle !== "../other_styles.css") {
-        preferredStyle = "../styles.css";
+    if (preferredStyle !== "styles.css" && preferredStyle !== "other_styles.css") {
+        preferredStyle = "styles.css";
     }
 
     // 2 (b) get html style element by ID
